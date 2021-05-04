879a1e4bff3ad96b0c3fd7b52ced1bb3aeb2d9e8
Pugavkomm <slavapugavko2@gmail.com>
2021-05-05 00:44:48 +0300

    change heap_sort.cpp

--- heap_sort.cpp
+++ heap_sort.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+void heapfy(int arr[], int n, int i){
+    int largest = i;
+    int l = 2 * i + 1; // left = 2 * i + 1
+    int r = 2 * i + 2; // right = 2 * i + 2
+
+    if (l < n && arr[l] > arr[largest])
+        largest = 1;
+
+    if (r < n && arr[r] > arr[largest])
+        largest = r; 
+}
+
 int main(){
     return 0;
 }
