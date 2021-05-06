3f5dc5be4084ab9b4a0a04f02aa8edae241ecf02
Mechislav Pugavko <mech@manjaro.mts.local>
2021-05-06 14:18:50 +0300

    add delete node to tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -36,6 +36,14 @@ int main(){
     std::cout << "min: " << tree_min(tree)->data << '\n';
     std::cout << "max: " << tree_max(tree)->data << '\n';
     print(tree);
+	std::cout << "Delete min..." << '\n';
+	tree = tree_del_min(tree);
+	print(tree);
+	std::cout << "Delete max..." << '\n';
+	tree = tree_del_max(tree);
+	print(tree);
+	tree_del(tree, 4);
+	print(tree);
     tree = tree_destroy(tree);
     return 0;
 }
@@ -49,6 +57,30 @@ void tree_print(Node *tree){
     std::cout << tree->data << ' ';
     tree_print(tree->right);
     
+}
+
+Node *tree_del_max(Node* tree){
+	if (tree == NULL) return NULL;
+	Node *max = NULL; 
+	max = tree; 
+	while (max->right->right != NULL){
+		max = max->right;
+	}
+	free((void*)max->right);
+	max->right = NULL;
+	return tree; 
+}
+
+Node *tree_del_min(Node* tree){
+	if (tree == NULL) return NULL; 
+	Node *min = NULL;
+   	min = tree; 
+	while (min->left->left != NULL){
+		min = min->left;
+	}
+	free((void*)min->left );
+	min->left = NULL; 
+	return tree; 
 }
 
 Node *tree_min(Node* tree){
@@ -71,6 +103,35 @@ Node *tree_max(Node* tree){
     return max;
 }
 
+Node *tree_del(Node* tree, Data x){
+	if (tree == NULL) return NULL;
+
+	if (x < tree->data)
+		tree->left = tree_del(tree->left, x);
+	else if (x > tree->data)
+		tree->right = tree_del(tree->right, x);
+	else{
+		if (tree->left==NULL && tree->right==NULL){
+			free((void*) tree);
+			return NULL; 
+		}
+		else if (tree->right==NULL){
+			Node *temp = tree->left;
+			free((void*)tree); 
+			return temp;
+		}
+		else if (tree->left == NULL){
+			Node *temp = tree->right;
+			free((void*)tree);
+			return temp;
+		}
+		Node *temp = tree_min(tree->right);
+		tree->data = temp->data;
+		tree->right = tree_del(tree->right, temp->data);
+	}
+	return tree;
+	
+}
 
 Node *tree_search(Node *tree, Data x){
     if (tree == NULL) return NULL;
